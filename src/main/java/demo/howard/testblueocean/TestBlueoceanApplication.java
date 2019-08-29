6f2563437a39e0ff9b6d5343f6e47de930d5de69
Howard Wang <idontwannarock@gmail.com>
2019-08-29 09:24:37 +0800

    first commit

---
+++ src/main/java/demo/howard/testblueocean/TestBlueoceanApplication.java
@@ -0,0 +1,13 @@
+package demo.howard.testblueocean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBlueoceanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBlueoceanApplication.class, args);
+	}
+
+}
